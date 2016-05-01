7ec1383412d40c2a3391c060a9d38a552c991a77
Andre Miguel <ubrals@logan6.local>
2016-05-01 02:16:02 -0300

    CdU E1 - Adicionar Aluno

---
+++ test/org/fpij/jitakyoei/CdUE1AdicionarAlunoTest.java
@@ -0,0 +1,16 @@
+package org.fpij.jitakyoei;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.fpij.jitakyoei.model.beans.*;
+
+
+public class CdUE1AdicionarAlunoTest {
+    public CdUE1AdicionarAlunoTest (){
+    }
+    
+    @Test
+    public void testCriarAluno(){
+        Aluno aluno = new Aluno();
+    }
+}
